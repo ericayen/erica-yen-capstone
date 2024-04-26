7a784068e91e26375bce11c596135fc347f20073
ericayen <ahnuyan@gmail.com>
2024-04-26 15:37:58 -0700

    added header component with conditional rendering and responsive styling.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
+const logo = require("../../assets/Logo/logo.png");
 
 function Header() {
-  return <div>Header</div>;
+  const location = useLocation();
+  return (
+    <div className="w-dvw bg-white py-4 font-ms selection:bg-brown-light selection:text-white">
+      <nav className="leading-loose flex justify-center items-center space-x-1 overflow-scroll sm:space-x-2 lg:space-x-4">
+        {[
+          ["Home", "/"],
+          ["About", "/about"],
+          ["Beans", "/beans"],
+          ["Drinks", "/drinks"],
+          ["Quiz", "/quiz"],
+        ].map(([title, url]) => (
+          <>
+            <a
+              href={url}
+              className={`leading-loose rounded-full px-3 py-2 text-xs md:text-sm lg:text-base hover:bg-beige-light ${
+                location.pathname === url
+                  ? "text-brown-dark font-medium"
+                  : "text-brown"
+              } ${
+                location.pathname === url && url === "/"
+                  ? "underline underline-offset-8"
+                  : ""
+              }`}
+            >
+              <img
+                src={logo}
+                className={`${
+                  location.pathname === url && location.pathname !== "/"
+                    ? "inline w-4 animate-bounce mx-0.5 sm:w-6 lg:w-8"
+                    : "hidden"
+                }`}
+              />
+              {title}
+            </a>
+          </>
+        ))}
+      </nav>
+    </div>
+  );
 }
 
 export default Header;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+const logo = require("../../assets/Logo/logo.png");
 
 function HomePage() {
   //   const navigate = useNavigate();
@@ -21,17 +22,27 @@ function HomePage() {
   }, []);
 
   return (
-    <div className="px-2">
-      <div className="h-50 flex flex-col items-center justify-center content-between">
-        <div className="text-brown w-screen whitespace-nowrap overflow-hidden animate-typing font-ms text-2xl sm:text-4xl lg:text-6xl text-center">
+    <div className="px-2 bg-white min-h-dvh selection:bg-brown-light selection:text-white">
+      <div className="pt-28 flex flex-col items-center justify-center content-between">
+        <div className="leading-loose text-brown w-screen whitespace-nowrap overflow-hidden animate-typing font-ms text-2xl sm:text-4xl lg:text-5xl text-center">
           Welcome to{" "}
-          <span className="font-semibold text-brown-dark pe-2  text-3xl sm:text-6xl lg:text-8xl">
+          <span className="font-semibold text-brown-dark pe-2 text-3xl sm:text-6xl lg:text-7xl">
             Express-<span className="inline-block animate-bounce">o</span>
+            {/* <img
+              src={logo}
+              className="inline w-6 animate-bounce border border-1 rounded-full border-brown-dark sm:w-8 md:w-10 lg:w-12 sm:border-2"
+            /> */}
           </span>
         </div>
       </div>
+
       {showComponent && (
-        <div className="text-center text-brown-dark font-ms text-xs sm:text-sm lg:text-xl animate-fade">
+        <div className="pt-4 text-center text-brown-dark font-ms text-xs sm:text-sm lg:text-xl animate-fade">
+          <img
+            src={logo}
+            alt="coffee cup"
+            className="w-1/4 mx-auto py-4 lg:w-1/5"
+          />
           <p className="text-brown">
             <span className="text-brown-dark">/ikˈspresō/</span> : a coffee app
             to help you learn about the wonderful world of coffee
@@ -42,9 +53,10 @@ function HomePage() {
           </p>
         </div>
       )}
+
       {showButton && (
-        <div className="text-center font-ms py-4 animate-fade">
-          <button className="bg-brown text-white rounded-full px-3 py-1.5 sm:px-4 lg:px-5 text-xs sm:text-sm lg:text-xl font-medium hover:bg-white hover:text-brown hover:border-solid hover:border hover:border-brown active:scale-95">
+        <div className="text-center font-ms py-2 animate-fade">
+          <button className="bg-brown text-white rounded-full px-3 py-2 sm:px-4 lg:px-5 text-xs sm:text-sm lg:text-xl font-medium hover:bg-white hover:text-brown hover:border-solid hover:border hover:border-brown active:scale-95">
             Let's get roasting!
           </button>
         </div>
@@ -55,5 +67,5 @@ function HomePage() {
 
 export default HomePage;
 
-//selection:bg-fuchsia-300 selection:text-fuchsia-900
+//selection:bg-brown-light-300 selection:text-beige-light-900
 //to style highlighted/selected text
