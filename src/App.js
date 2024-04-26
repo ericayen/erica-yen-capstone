989fcf059868d8ec856f706d9b530bbd2b293d80
ericayen <ahnuyan@gmail.com>
2024-04-26 15:56:54 -0700

    created footer component & responsive styling. fixed bug in header.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import "./App.css";
 import HomePage from "./pages/HomePage/HomePage";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         {/* <Route path="/about" /> */}
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
