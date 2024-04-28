9e821131930aa71b4bd9c6c64a9dd650d7053c1d
ericayen <ahnuyan@gmail.com>
2024-04-28 14:58:15 -0700

    created Beans Page. imported Daisy UI.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
+import BeansPage from "./pages/BeansPage";
 import Footer from "./components/Footer";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
+        <Route path="/beans" element={<BeansPage />} />
       </Routes>
       <Footer />
     </BrowserRouter>
