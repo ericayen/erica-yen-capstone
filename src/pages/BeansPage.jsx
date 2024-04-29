3e51d532785cc82e637bd90cca6c56bd4dc857ed
ericayen <ahnuyan@gmail.com>
2024-04-29 12:29:53 -0700

    refactored code to display data using CoffeeCard component

--- src/pages/BeansPage.jsx
+++ src/pages/BeansPage.jsx
@@ -1,15 +1,24 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { apiUrl } from "../utils";
+import CoffeeCard from "../components/CoffeeCard";
 
 function BeansPage() {
-  const [coffeeData, setCoffeeData] = useState([]);
+  //   const [coffeeData, setCoffeeData] = useState([]);
+  const [arabicaData, setArabicaData] = useState([]);
+  const [robustaData, setRobustaData] = useState([]);
+  const [libericaData, setLibericaData] = useState({});
+  const [excelsaData, setExcelsaData] = useState({});
 
   const getBeans = async () => {
     try {
       const response = await axios.get(`${apiUrl}/coffee`);
       const data = response.data;
-      setCoffeeData(data);
+      //   setCoffeeData(data);
+      setArabicaData(data.slice(0, 5));
+      setRobustaData(data.slice(5, 8));
+      setLibericaData(data[8]);
+      setExcelsaData(data[9]);
     } catch (error) {
       console.error("Error getting coffee data", error);
     }
@@ -20,13 +29,24 @@ function BeansPage() {
     getBeans();
   }, []);
 
-  const arabica = coffeeData.slice(0, 5);
-  const robusta = coffeeData.slice(5, 8);
-  const liberica = coffeeData[8];
-  const excelsa = coffeeData[9];
+  //   console.log(libericaData);
+  //   console.log(robustaData);
+
+  //   const arabica = coffeeData.slice(0, 5);
+  //   const robusta = coffeeData.slice(5, 8);
+  //   const liberica = coffeeData[8];
+  //   const excelsa = coffeeData[9];
+
+  //   function getHistory(coffee) {
+  //     if (coffee.length > 0 && coffee[0].history) {
+  //       return coffee[0].history;
+  //     } else {
+  //       return "History data not available";
+  //     }
+  //   }
 
   return (
-    <main className="w-screen px-8 leading-loose text-center bg-white sm:px-16 lg:px-36 xl:px-48 2xl:px-96 min-h-dvh font-ms text-brown-dark selection:bg-brown-light selection:text-white">
+    <main className="w-screen px-8 leading-loose text-center bg-white sm:px-24 lg:px-36 min-h-dvh font-ms text-brown-dark selection:bg-brown-light selection:text-white">
       <h1 className="pb-12 text-2xl lg:pb-28 sm:text-4xl lg:text-5xl">
         <span className="text-brown">About </span>
         <span className="font-semibold">Beans</span>
@@ -34,66 +54,86 @@ function BeansPage() {
 
       <section>
         <div className="mb-2 collapse collapse-plus bg-beige-light">
-          <input type="radio" name="my-accordion-3" defaultChecked />
-          <div className="text-xl font-medium collapse-title">Arabica</div>
-          <div className="collapse-content">
-            <div className="w-full carousel h-50">
-              {arabica.map((coffee) => (
-                <div id={coffee.coffee_id} className="w-full carousel-item">
-                  <div className="bg-white shadow-xl card">
-                    <p>{coffee.coffee_type}</p>
-                  </div>
-                </div>
-              ))}
-            </div>
-            <div className="flex justify-center w-full gap-2 py-2">
-              <a href="#1" className="btn btn-xs">
-                1
-              </a>
-              <a href="#2" className="btn btn-xs">
-                2
-              </a>
-              <a href="#3" className="btn btn-xs">
-                3
-              </a>
-              <a href="#4" className="btn btn-xs">
-                4
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="mb-2 collapse collapse-plus bg-base-200">
           <input type="radio" name="my-accordion-3" />
-          <div className="text-xl font-medium collapse-title">Robusta</div>
-          <div className="collapse-content">
-            <div className="shadow-xl card lg:card-side bg-base-100">
-              <figure>
-                <img
-                  src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg"
-                  alt="Album"
-                />
-              </figure>
+          <h2 className="text-lg font-medium collapse-title">Arabica</h2>
+          <CoffeeCard arabicaData={arabicaData} />
+          {/* <div className="collapse-content ">
+            <div className="w-full bg-white card lg:card-side">
               <div className="card-body">
-                <h2 className="card-title">New album is released!</h2>
-                <p>Click the button to listen on Spotiwhy app.</p>
+                <h3 className="flex items-center justify-center text-lg font-normal card-title"></h3>
+                <p className="text-sm">{getHistory(arabica)}</p>
               </div>
             </div>
-          </div>
+            <div className="flex justify-center w-full gap-2 py-2">
+              <a
+                href="#1"
+                className="bg-white border-none btn btn-xs hover:bg-brown-light"
+              >
+                1
+              </a>
+              {[...Array(arabica.length)].map((_, index) => (
+                <a
+                  key={index}
+                  href={`#${index + 2}`}
+                  className="border-none bg-beige-light btn btn-xs hover:bg-brown-light"
+                >
+                  {index + 2}
+                </a>
+              ))}
+            </div>
+          </div> */}
         </div>
-        <div className="mb-2 collapse collapse-plus bg-base-200">
+
+        <div className="mb-2 collapse collapse-plus bg-beige-light">
           <input type="radio" name="my-accordion-3" />
-          <div className="text-xl font-medium collapse-title">Liberica</div>
-          <div className="collapse-content">
-            <p>hello</p>
-          </div>
+          <h2 className="text-lg font-medium collapse-title">Robusta</h2>
+          <CoffeeCard robustaData={robustaData} />
+          {/* <div className="collapse-content ">
+            <div className="w-full bg-white card lg:card-side">
+              <div className="card-body">
+                <h3 className="flex items-center justify-center text-lg font-normal card-title"></h3>
+                <p className="text-sm">{getHistory(robusta)}</p>
+              </div>
+            </div>
+            <div className="flex justify-center w-full gap-2 py-2">
+              <a
+                href="#1"
+                className="bg-white border-none btn btn-xs hover:bg-brown-light"
+              >
+                1
+              </a>
+              {[...Array(robusta.length)].map((_, index) => (
+                <a
+                  key={index}
+                  href={`#${index + 2}`}
+                  className="border-none bg-beige-light btn btn-xs hover:bg-brown-light"
+                >
+                  {index + 2}
+                </a>
+              ))}
+            </div>
+          </div> */}
         </div>
-        <div className="mb-2 collapse collapse-plus bg-base-200">
+
+        <div className="mb-2 collapse collapse-plus bg-beige-light">
+          <input type="radio" name="my-accordion-3" />
+          <h2 className="text-lg font-medium collapse-title">Liberica</h2>
+          <CoffeeCard libericaData={libericaData} />
+        </div>
+
+        <div className="mb-2 collapse collapse-plus bg-beige-light">
+          <input type="radio" name="my-accordion-3" />
+          <h2 className="text-lg font-medium collapse-title">Excelsa</h2>
+          <CoffeeCard excelsaData={excelsaData} />
+        </div>
+
+        {/* <div className="mb-2 collapse collapse-plus bg-base-200">
           <input type="radio" name="my-accordion-3" />
           <div className="text-xl font-medium collapse-title">Excelsa</div>
           <div className="collapse-content">
             <p>hello</p>
           </div>
-        </div>
+        </div> */}
       </section>
     </main>
   );
