569f0cf4230b2b2850a68c30eefc51d39d1611b9
ericayen <ahnuyan@gmail.com>
2024-05-01 12:02:14 -0700

    refactored Quiz Page code. deleted Quiz Start Page & created Quiz component.

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,120 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { apiUrl } from "../utils";
+
+function Quiz() {
+  const [questions, setQuestions] = useState([]);
+  const [responses, setResponses] = useState([]);
+  const [answers, setAnswers] = useState({});
+
+  const getQuestions = async () => {
+    try {
+      const response = await axios.get(`${apiUrl}/questions`);
+      const data = response.data;
+      setQuestions(data);
+    } catch (error) {
+      console.error("Error getting quiz questions:", error);
+    }
+  };
+
+  const getResponses = async () => {
+    try {
+      const response = await axios.get(`${apiUrl}/responses`);
+      const data = response.data;
+      setResponses(data);
+    } catch (error) {
+      console.error("Error getting quiz responses:", error);
+    }
+  };
+
+  useEffect(() => {
+    document.title = "Take a Quiz";
+    getQuestions();
+    getResponses();
+  }, []);
+
+  const handleResponseChange = (questionId, selectedOption) => {
+    const selectedResponse = responses.find(
+      (response) =>
+        response.question_id === questionId &&
+        response.option === selectedOption
+    );
+    setAnswers((prevAnswers) => ({
+      ...prevAnswers,
+      [questionId]: selectedResponse,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!answers) {
+      alert("Please answer all the questions!");
+      return;
+    }
+    let totalWeight = 0;
+    Object.values(answers).forEach((answer) => {
+      if (answer) {
+        totalWeight += answer.weight;
+      }
+    });
+    console.log("Total weight:", totalWeight);
+    setAnswers([]);
+    e.target.reset();
+  };
+
+  console.log(answers);
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        {questions.map((question) => (
+          <div key={question.question_id}>
+            <h3 className="pt-8 pb-2 text-sm font-medium sm:text-lg lg:text-xl">
+              {question.question}
+            </h3>
+            <ul>
+              {responses
+                .filter(
+                  (response) => response.question_id === question.question_id
+                )
+                .map((response) => (
+                  <li
+                    key={response.response_id}
+                    className="flex items-center justify-center "
+                  >
+                    <input
+                      type="radio"
+                      className="mr-2 radio radio-xs checked:bg-red"
+                      id={response.response_id}
+                      name={`question_${question.question_id}`}
+                      value={response.option}
+                      onChange={(e) =>
+                        handleResponseChange(
+                          question.question_id,
+                          e.target.value
+                        )
+                      }
+                      required
+                    />
+                    <label
+                      htmlFor={response.response_id}
+                      className="cursor-pointer"
+                    >
+                      {response.option}
+                    </label>
+                  </li>
+                ))}
+            </ul>
+          </div>
+        ))}
+        <button
+          type="submit"
+          className="px-3 py-2 mt-4 text-sm font-medium text-white border border-solid rounded-full w-fit bg-brown border-brown sm:px-4 lg:px-5 lg:text-base hover:bg-white hover:text-brown hover:border-solid hover:border hover:border-brown active:scale-95"
+        >
+          Submit
+        </button>
+      </form>
+    </>
+  );
+}
+
+export default Quiz;
--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -1,123 +1,31 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { apiUrl } from "../utils";
-import { useNavigate } from "react-router-dom";
+import Quiz from "../components/Quiz";
 
-function QuizPage() {
-  const navigate = useNavigate();
-  const [questions, setQuestions] = useState([]);
-  const [responses, setResponses] = useState([]);
-  const [answers, setAnswers] = useState({});
+function QuizStartPage() {
+  const [clicked, setClicked] = useState(false);
 
-  const getQuestions = async () => {
-    try {
-      const response = await axios.get(`${apiUrl}/questions`);
-      const data = response.data;
-      setQuestions(data);
-    } catch (error) {
-      console.error("Error getting quiz questions:", error);
-    }
-  };
-
-  const getResponses = async () => {
-    try {
-      const response = await axios.get(`${apiUrl}/responses`);
-      const data = response.data;
-      setResponses(data);
-    } catch (error) {
-      console.error("Error getting quiz responses:", error);
-    }
-  };
-
-  useEffect(() => {
-    document.title = "Take a Quiz";
-    getQuestions();
-    getResponses();
-  }, []);
-
-  const handleResponseChange = (questionId, selectedOption) => {
-    const selectedResponse = responses.find(
-      (response) =>
-        response.question_id === questionId &&
-        response.option === selectedOption
-    );
-    setAnswers((prevAnswers) => ({
-      ...prevAnswers,
-      [questionId]: selectedResponse,
-    }));
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!answers) {
-      alert("Please answer all the questions!");
-      return;
-    }
-    let totalWeight = 0;
-    Object.values(answers).forEach((answer) => {
-      if (answer) {
-        totalWeight += answer.weight;
-      }
-    });
-    console.log("Total weight:", totalWeight);
-    setAnswers([]);
-    e.target.reset();
-  };
-
-  console.log(answers);
+  const handleClick = () => setClicked(true);
 
   return (
     <main className="w-screen px-8 leading-loose text-center bg-white sm:px-24 lg:px-36 min-h-dvh font-ms text-brown-dark selection:bg-brown-light selection:text-white">
-      <form onSubmit={handleSubmit}>
-        {questions.map((question) => (
-          <div key={question.question_id}>
-            <h3 className="pt-8 pb-2 text-sm font-medium sm:text-lg lg:text-xl">
-              {question.question}
-            </h3>
-            <ul>
-              {responses
-                .filter(
-                  (response) => response.question_id === question.question_id
-                )
-                .map((response) => (
-                  <li
-                    key={response.response_id}
-                    className="flex items-center justify-center "
-                  >
-                    <input
-                      type="radio"
-                      className="mr-2 radio radio-xs checked:bg-red"
-                      id={response.response_id}
-                      name={`question_${question.question_id}`}
-                      value={response.option}
-                      onChange={(e) =>
-                        handleResponseChange(
-                          question.question_id,
-                          e.target.value
-                        )
-                      }
-                      required
-                    />
-                    <label
-                      htmlFor={response.response_id}
-                      className="cursor-pointer"
-                    >
-                      {response.option}
-                    </label>
-                  </li>
-                ))}
-            </ul>
-          </div>
-        ))}
-        <button
-          type="submit"
-          className="px-3 py-2 mt-4 text-sm font-medium text-white border border-solid rounded-full w-fit bg-brown border-brown sm:px-4 lg:px-5 lg:text-base hover:bg-white hover:text-brown hover:border-solid hover:border hover:border-brown active:scale-95"
-        >
-          Submit
-        </button>
-      </form>
+      <h1 className="pb-12 text-2xl lg:pb-28 sm:text-4xl lg:text-5xl">
+        <span className="text-brown">Take a </span>
+        <span className="font-semibold">Quiz</span>
+      </h1>
+      <h2 className="text-base sm:text-lg lg:text-2xl ">
+        Find out what coffee best matches your preferences
+      </h2>
+      <button
+        onClick={() => handleClick()}
+        className="px-3 py-2 mt-8 mb-12 text-sm font-medium text-white border border-solid rounded-full lg:mb-28 w-fit bg-brown border-brown sm:px-4 lg:px-5 lg:text-base hover:bg-white hover:text-brown hover:border-solid hover:border hover:border-brown active:scale-95"
+      >
+        Let's go!
+      </button>
+      {clicked && <Quiz />}
     </main>
   );
 }
 
-export default QuizPage;
+export default QuizStartPage;
