0b931c45065401ca16ae52c6cc7538f59e6376ee
ericayen <ahnuyan@gmail.com>
2024-05-06 15:10:38 -0700

    added error state handling. refactored code to stay DRY

--- src/pages/CommentsPage.jsx
+++ src/pages/CommentsPage.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { v4 as uuidv4 } from "uuid";
 import { apiUrl } from "../utils";
+import InputField from "../components/InputField";
 import CommentCard from "../components/CommentCard";
 
 function CommentsPage() {
@@ -10,7 +11,7 @@ function CommentsPage() {
     name: "",
     comment: "",
   });
-  //   const [formErrors, setFormErrors] = useState({});
+  const [formErrors, setFormErrors] = useState({});
 
   const getComments = async () => {
     const response = await axios.get(`${apiUrl}/comments`);
@@ -24,17 +25,28 @@ function CommentsPage() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (!formState.name.trim() || !formState.comment.trim()) {
+    const errors = {};
+    if (!formState.name.trim()) {
+      errors.name = "Name is required";
+    }
+    if (!formState.comment.trim()) {
+      errors.comment = "Comment is required";
+    }
+
+    if (Object.keys(errors).length > 0) {
       alert("Please fill out all the fields!");
+      setFormErrors(errors);
       return;
     }
+
+    setFormErrors({});
 
     const newComment = {
       comment_id: uuidv4(),
       name: formState.name,
       comment: formState.comment,
       date: Date.now(),
-      avatar: null,
+      avatar: null, //this will display dynamically once user profile & auth is implemented
     };
 
     try {
@@ -52,10 +64,6 @@ function CommentsPage() {
     }
   };
 
-  //   const validateForm = () => {
-  //     let errors = {};
-  //   };
-
   return (
     <main>
       <h1>
@@ -69,34 +77,28 @@ function CommentsPage() {
       >
         <div className="items-center w-full pt-4 pb-6 mt-4 bg-white rounded-2xl card-body">
           <div className="w-full">
-            <label className="w-full form-control">
-              <div className="label">
-                <span className="font-medium label-text">Name</span>
-              </div>
-              <input
-                type="text"
-                value={formState.name}
-                onChange={(e) =>
-                  setFormState({ ...formState, name: e.target.value })
-                }
-                placeholder="Type here..."
-                className="w-full py-2 border input border-beige-light focus:outline-green focus:border-green h-fit"
-              />
-            </label>
-            <label className="w-full form-control">
-              <div className="label">
-                <span className="font-medium label-text">Comment</span>
-              </div>
-              <input
-                type="text"
-                value={formState.comment}
-                onChange={(e) =>
-                  setFormState({ ...formState, comment: e.target.value })
-                }
-                placeholder="Type here..."
-                className="w-full py-2 border input border-beige-light focus:outline-green focus:border-green h-fit"
-              />
-            </label>
+            <InputField
+              type="text"
+              name="name"
+              label="Name"
+              value={formState.name}
+              error={formErrors.name}
+              onChange={(e) =>
+                setFormState({ ...formState, name: e.target.value })
+              }
+              placeholder="Type here..."
+            />
+            <InputField
+              type="text"
+              name="comment"
+              label="Comment"
+              value={formState.comment}
+              error={formErrors.comment}
+              onChange={(e) =>
+                setFormState({ ...formState, comment: e.target.value })
+              }
+              placeholder="Type here..."
+            />
           </div>
           <div className="w-full sm:text-end">
             <button type="submit" className="w-full mt-3 lg:w-fit button">
